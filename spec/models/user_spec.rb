d3499b1c31ad2fe91c5850c2fc4f991a93403b75
Lucas Mercado <luke.isla@gmail>
2015-03-28 04:06:21 -0700

    Cleaned up the user's table and ran annotate.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,30 +2,18 @@
 #
 # Table name: users
 #
-#  id                     :integer          not null, primary key
-#  email                  :string           default(""), not null
-#  encrypted_password     :string           default(""), not null
-#  reset_password_token   :string
-#  reset_password_sent_at :datetime
-#  remember_created_at    :datetime
-#  sign_in_count          :integer          default("0"), not null
-#  current_sign_in_at     :datetime
-#  last_sign_in_at        :datetime
-#  current_sign_in_ip     :inet
-#  last_sign_in_ip        :inet
-#  confirmation_token     :string
-#  confirmed_at           :datetime
-#  confirmation_sent_at   :datetime
-#  unconfirmed_email      :string
-#  failed_attempts        :integer          default("0"), not null
-#  unlock_token           :string
-#  locked_at              :datetime
-#  created_at             :datetime
-#  updated_at             :datetime
-#  provider               :string
-#  uid                    :string
-#  main_character_name    :string
-#  main_character_id      :integer
+#  id                  :integer          not null, primary key
+#  sign_in_count       :integer          default("0"), not null
+#  current_sign_in_at  :datetime
+#  last_sign_in_at     :datetime
+#  current_sign_in_ip  :inet
+#  last_sign_in_ip     :inet
+#  created_at          :datetime
+#  updated_at          :datetime
+#  provider            :string
+#  uid                 :string
+#  main_character_name :string
+#  main_character_id   :integer
 #
 
 require 'rails_helper'

02336487e5ce6231a5877567d3f8f35ffc0f5104
Lucas Mercado <luke.isla@gmail>
2015-03-27 18:23:51 -0700

    Refactored OmniauthCallbacksController to better handle non-authorized logins.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,7 +3,12 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     puts "eve_crest_sso called"
     puts request.env["omniauth.auth"]
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    sign_in_and_redirect @user
+    if @user.present?
+      sign_in_and_redirect @user
+    else
+      flash[:alert] = "#{request.env["omniauth.auth"].info["name"]} is not a known member of this organization."
+      redirect_to new_user_session_path
+    end
     # Appears to handle processing of the data returned from omniauth
   end
 end

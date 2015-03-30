b7ac50b97e0505c1ea7d5377cfa70bacfac65522
Lucas Mercado <luke.isla@gmail>
2015-03-30 12:01:32 -0700

    Swapped to authenticated_root & non-authenticated root routes.

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,15 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   devise_scope :user do
-    root to: "devise/sessions#new"
     get 'sign_in', :to => 'devise/sessions#new', :as => :new_user_session
     get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
   end
+
+  authenticated :user do
+    root to: "videos#latest", as: :authenticated_root
+  end
+
+  root to: redirect('/sign_in')
 
   resources :videos
   get 'latest/videos' => 'videos#latest'

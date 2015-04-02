f810a376a6add27ef58a878d544a4255f7858f5b
Lucas Mercado <luke.isla@gmail>
2015-04-02 15:01:09 -0700

    Migrated in the doctrines table.

--- db/migrate/20150402215729_create_doctrines.rb
+++ db/migrate/20150402215729_create_doctrines.rb
@@ -4,7 +4,7 @@ class CreateDoctrines < ActiveRecord::Migration
       t.string :name
       t.string :short_description
       t.text :long_description
-      t.bool :access_by_hierarchy
+      t.boolean :access_by_hierarchy
 
       t.timestamps null: false
     end

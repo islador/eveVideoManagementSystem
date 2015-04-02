901f5ecf4dcd240dc1bf05988ca2fc01e4eb9312
Lucas Mercado <luke.isla@gmail>
2015-04-02 14:26:06 -0700

    Fixed an issue in user#director method where only directors would receive a true return as opposed to the intended directors & ceos.

--- app/models/user.rb
+++ app/models/user.rb
@@ -96,6 +96,10 @@ class User < ActiveRecord::Base
   end
 
   def director
-    Role.where(name: ["Corp Director", "Corp CEO"])[0].users.where("user_id = ?", self.id)[0].present?
+    # Retrieve the director and ceo role IDs from the database
+    director_ceo_role_ids = Role.where(name: ["Corp Director", "Corp CEO"]).pluck(:id)
+
+    # See if the user has those roles.
+    RolesUser.where(role_id: director_ceo_role_ids, user_id: self.id)[0].present?
   end
 end

7abec2ae9a4c7f6acf1ea1182203e4039cacaf03
Lucas Mercado <luke.isla@gmail>
2015-04-02 15:10:26 -0700

    Ran Annotate.

--- spec/models/doctrine_spec.rb
+++ spec/models/doctrine_spec.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: doctrines
+#
+#  id                  :integer          not null, primary key
+#  name                :string
+#  short_description   :string
+#  long_description    :text
+#  access_by_hierarchy :boolean
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe Doctrine, type: :model do
--- spec/models/doctrines_role_spec.rb
+++ spec/models/doctrines_role_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: doctrines_roles
+#
+#  id          :integer          not null, primary key
+#  role_id     :integer
+#  doctrine_id :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe DoctrinesRole, type: :model do

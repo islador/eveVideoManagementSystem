668ceb86a07019993872a44e1dedc2f2da1cfffb
Lucas Mercado <luke.isla@gmail>
2015-04-03 13:27:08 -0700

    Ran annotate.

--- app/models/fitting.rb
+++ app/models/fitting.rb
@@ -1,2 +1,20 @@
+# == Schema Information
+#
+# Table name: fittings
+#
+#  id                   :integer          not null, primary key
+#  name                 :string
+#  hull                 :string
+#  race                 :string
+#  fleet_role           :string
+#  description          :text
+#  progression          :string
+#  progression_position :integer
+#  eft_string           :string
+#  ship_dna             :string
+#  created_at           :datetime         not null
+#  updated_at           :datetime         not null
+#
+
 class Fitting < ActiveRecord::Base
 end
--- spec/models/fitting_spec.rb
+++ spec/models/fitting_spec.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: fittings
+#
+#  id                   :integer          not null, primary key
+#  name                 :string
+#  hull                 :string
+#  race                 :string
+#  fleet_role           :string
+#  description          :text
+#  progression          :string
+#  progression_position :integer
+#  eft_string           :string
+#  ship_dna             :string
+#  created_at           :datetime         not null
+#  updated_at           :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe Fitting, type: :model do

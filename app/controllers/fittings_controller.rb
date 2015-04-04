98288aa18b0983abfaca6ce76c80589556ffa637
Lucas Mercado <luke.isla@gmail>
2015-04-03 17:26:59 -0700

    Fleshed out a basic fittings#new method and page.

--- app/controllers/fittings_controller.rb
+++ app/controllers/fittings_controller.rb
@@ -1,21 +1,33 @@
 class FittingsController < ApplicationController
   def index
-    @fittings = Fittings.where(doctrine_id: params[:doctrine_id])
+    @fittings = Fitting.where(doctrine_id: params[:doctrine_id])
     authorize @fittings[0]
   end
 
   def create
-    @fitting = Fitting.new(create_fitting_params, doctrine_id: params[:doctrine_id])
+    @fitting = Fitting.new(create_fitting_params)
     authorize @fitting
+    # Split the EFT String apart line by line
+    eft_string = params[:eft_string]
+    eft_string_components = eft_string.split["\n"]
+
+    # Extract the hull and name data from the first line
+    info_bracket = eft_string_components[0].slice(1..eft_string_components[0].length-2).split(",")
+    @fitting.hull = info_bracket[0]
+    @fitting.name = info_bracket[1]
+
     if @fitting.save
-      redirect_to doctrine_fittings_path(@fitting)
+      doctrine = @fitting.doctrine
+      redirect_to doctrine_path(@fitting.doctrine)
     else
+      @doctrine = Doctrine.find(params[:doctrine_id])
       render 'new'
     end
   end
 
   def new
-    @fitting = Fitting.new(doctrine_id: params[:doctrine_id])
+    @doctrine = Doctrine.find(params[:doctrine_id])
+    @fitting = Fitting.new(doctrine_id: @doctrine.id)
     authorize @fitting
   end
 
@@ -51,6 +63,6 @@ class FittingsController < ApplicationController
 
   private
     def create_fitting_params
-      params.permit(:name, :hull, :race, :fleet_role, :description, :progression, :progression_position, :eft_string)
+      params.permit(:name, :hull, :race, :fleet_role, :description, :progression, :progression_position, :eft_string, :doctrine_id)
     end
 end

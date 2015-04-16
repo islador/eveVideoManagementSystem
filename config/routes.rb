9dca75ec862d32d897733498a28041a6b880387a
Lucas Mercado <luke.isla@gmail>
2015-04-16 15:09:58 -0700

    Fixed a routing problem around accessible_agents. Added a nav link to accessible agents from the missions index.

--- config/routes.rb
+++ config/routes.rb
@@ -37,5 +37,5 @@ Rails.application.routes.draw do
   get 'refresh_fac_war_systems' => 'fac_war_systems#refresh_fac_war_systems'
 
   resources :missions, only: [:index, :show, :new, :create, :destroy]
-  get 'missions/accessible_agents'
+  get 'accessible_agents' => 'missions#accessible_agents'
 end
